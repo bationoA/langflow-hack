11f491c05d193c026fbd23e5ff2239439f1c9679
bationoA <67708432+bationoA@users.noreply.github.com>
2024-07-14 14:13:30 +0300

    Update flow1.py

--- flow/flow1.py
+++ flow/flow1.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 
 from typing import List
 
@@ -11,8 +12,13 @@ load_dotenv(".env")
 flow_file_path = os.path.join("database", "TrustNode Care.json")
 
 
-
 def get_flow1_response(user_message: str) -> list[RunOutputs]:
+    if os.getenv('ASTRA_DB_ENDPOINT', None) is None or os.getenv('ASTRA_DB_TOKEN', None) is None:
+        # If None, then we supposed this program is running on Streamlit. So, we get the environment
+        # variables from Streamlit built-in env variables manager
+        os.environ['ASTRA_DB_ENDPOINT'] = st.secrets["ASTRA_DB_ENDPOINT"]
+        os.environ['ASTRA_DB_TOKEN'] = st.secrets["ASTRA_DB_TOKEN"]
+
     TWEAKS = {
         "ChatInput-UYsRC": {},
         "Prompt-pMN5s": {},
